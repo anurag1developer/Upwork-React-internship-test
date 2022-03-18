3b01036827dfd381e760f5c8e2ea3b88eb643c9a
Anurag Budakoti <anuragbudakoti2@gmail.com>
2022-03-18 19:24:27 +0530

    some new styles added

--- src/components/Mainbar/Sections.js
+++ src/components/Mainbar/Sections.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./Sections.css";
 
+import { IconButton } from "@mui/material";
+
 import { ArrowBack } from "@mui/icons-material";
 
 import SectionList from "./SectionList";
@@ -9,7 +11,9 @@ import SectionList from "./SectionList";
 const Sections = () => {
   return (
     <div className="sections">
-      <ArrowBack />
+      <IconButton>
+        <ArrowBack />
+      </IconButton>
       <div className="sections__categories">
         <SectionList />
       </div>
--- src/components/Mainbar/Topbar.js
+++ src/components/Mainbar/Topbar.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./Topbar.css";
 
+import { IconButton } from "@mui/material";
+
 import {
   NotificationsNoneOutlined,
   MailOutlineOutlined,
@@ -14,14 +16,22 @@ const Topbar = () => {
       <div className="topbarWrapper">
         <h2 className="topbar__title">Dashboard</h2>
         <div className="topbar__options">
-          <NotificationsNoneOutlined className="topbar__notification" />
-          <MailOutlineOutlined className="topbar__mail" />
-          <SettingsOutlined className="topbar__settings" />
-          <img
-            className="topbar__profile"
-            src="assets/profile.png"
-            alt="profile"
-          />
+          <IconButton>
+            <NotificationsNoneOutlined className="topbar__notification" />
+          </IconButton>
+          <IconButton>
+            <MailOutlineOutlined className="topbar__mail" />
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined className="topbar__settings" />
+          </IconButton>
+          <IconButton>
+            <img
+              className="topbar__profile"
+              src="assets/profile.png"
+              alt="profile"
+            />
+          </IconButton>
         </div>
       </div>
     </div>
