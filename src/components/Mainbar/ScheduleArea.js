7c6e16d6c196b56c0fb7fe08f42540d14a320d99
Anurag Budakoti <anuragbudakoti2@gmail.com>
2022-03-18 19:08:37 +0530

    styling of select option html element

--- src/components/Mainbar/ScheduleArea.js
+++ src/components/Mainbar/ScheduleArea.js
@@ -8,9 +8,12 @@ const ScheduleArea = () => {
       <div className="schedule__left">
         <p className="schedule__text">Select</p>
         <div className="schedule__dash"></div>
-        <select className="schedule__dropdown" name="test" id="test">
-          <option value="Past Test">Past Test</option>
-        </select>
+        <div className="schedule__styling">
+          <select className="schedule__dropdown" name="test" id="test">
+            <option value="Past Test">Past Test</option>
+          </select>
+          <label htmlFor="test" className="extra"></label>
+        </div>
       </div>
       <button className="schedule__test">Schedule new Test</button>
     </div>
